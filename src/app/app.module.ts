2b32bddcea491b40c42ea3092a145fb9322ed1ff
MSI\何致緯 <a0983393895@gmail.com>
2021-01-30 14:46:18 +0800

    #1 fromTable初次建立

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Menu1Component } from './model/menu/menu1/menu1.component';
+import { Template01Component } from './model/component/template01/template01.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Menu1Component,
+    Template01Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/component/template01/template01.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template01',
+  templateUrl: './template01.component.html',
+  styleUrls: ['./template01.component.scss']
+})
+export class Template01Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
